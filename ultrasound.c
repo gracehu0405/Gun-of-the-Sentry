cb87f2ce67762829f7513ad60488f2225648a686
Michael <mcoduoza@stanford.edu>
2019-03-14 17:56:18 -0700

    Major cleanup of ultrasound code

--- ultrasound.c
+++ ultrasound.c
@@ -24,96 +24,38 @@ static volatile unsigned start_times[NUM_SENSORS];
 static volatile unsigned end_times[NUM_SENSORS];
 static volatile unsigned int distances[NUM_SENSORS];
 
-static unsigned int currSensorNum;
+static unsigned int currSensorNum = 0;
 
 static void send_new_pulse(unsigned int pin);
 
 
-bool risingEdge_0(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[0].echo_rising_pin)){
-        start_times[0] = timer_get_ticks();
+bool risingEdge(unsigned int pc){
+    if(gpio_check_and_clear_event(sensor_array[currSensorNum].echo_rising_pin)){
+        start_times[currSensorNum] = timer_get_ticks();
         return true;
     }
     return false;
 }
 
 
-bool fallingEdge_0(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[0].echo_falling_pin)){
-        end_times[0] = timer_get_ticks();
-        distances[0] = (end_times[0] - start_times[0]) / 149;
-        
-        // disable sensor 0
-        gpio_disable_event_detection(sensor_array[0].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_disable_event_detection(sensor_array[0].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
+bool fallingEdge(unsigned int pc){
+    if(gpio_check_and_clear_event(sensor_array[currSensorNum].echo_falling_pin)){
+        end_times[currSensorNum] = timer_get_ticks();
+        distances[currSensorNum] = (end_times[currSensorNum] - start_times[currSensorNum]) / 149;
 
-        // enable sensor 1 
-        gpio_enable_event_detection(sensor_array[1].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_enable_event_detection(sensor_array[1].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
+        // disable the current sensor
+        gpio_disable_event_detection(sensor_array[currSensorNum].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
+        gpio_disable_event_detection(sensor_array[currSensorNum].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
 
-        // send out sensor 1 pulse 
-        send_new_pulse(sensor_array[1].trigger);
+        // Advance to the next sensor
+        currSensorNum = ((currSensorNum + 1) % NUM_SENSORS);
 
-        return true;
-    }
-    return false;
-}
+        // enable the next sensor
+        gpio_enable_event_detection(sensor_array[currSensorNum].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
+        gpio_enable_event_detection(sensor_array[currSensorNum].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
 
-
-bool risingEdge_1(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[1].echo_rising_pin)){
-        start_times[1] = timer_get_ticks();
-        return true;
-    }
-    return false;
-}
-
-
-bool fallingEdge_1(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[1].echo_falling_pin)){
-        end_times[1] = timer_get_ticks();
-        distances[1] = (end_times[1] - start_times[1]) / 149;
-        
-        // disable sensor 1
-        gpio_disable_event_detection(sensor_array[1].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_disable_event_detection(sensor_array[1].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-
-        // enable sensor 2
-        gpio_enable_event_detection(sensor_array[2].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_enable_event_detection(sensor_array[2].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-
-        // send out sensor 2 pulse 
-        send_new_pulse(sensor_array[2].trigger);
-
-        return true;
-    }
-    return false;
-}
-
-bool risingEdge_2(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[2].echo_rising_pin)){
-        start_times[2] = timer_get_ticks();
-        return true;
-    }
-    return false;
-}
-
-
-bool fallingEdge_2(unsigned int pc){
-    if(gpio_check_and_clear_event(sensor_array[2].echo_falling_pin)){
-        end_times[2] = timer_get_ticks();
-        distances[2] = (end_times[2] - start_times[2]) / 149;
-
-        // disable sensor 2
-        gpio_disable_event_detection(sensor_array[2].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_disable_event_detection(sensor_array[2].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-
-        // enable sensor 0
-        gpio_enable_event_detection(sensor_array[0].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-        gpio_enable_event_detection(sensor_array[0].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-
-        // send out sensor 0 pulse 
-        send_new_pulse(sensor_array[0].trigger);
+        // send out a the next sensor pulse 
+        send_new_pulse(sensor_array[currSensorNum].trigger);
 
         return true;
     }
@@ -127,65 +69,37 @@ static void send_new_pulse(unsigned int pin){
 }
 
 static void interrupts_init(void){
-   gpio_enable_event_detection(sensor_array[0].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-   gpio_enable_event_detection(sensor_array[0].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-   
-   interrupts_attach_handler(risingEdge_0);
-   interrupts_attach_handler(fallingEdge_0);
+    gpio_enable_event_detection(sensor_array[0].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
+    gpio_enable_event_detection(sensor_array[0].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
 
+    interrupts_attach_handler(risingEdge);
+    interrupts_attach_handler(fallingEdge);
 
-  // gpio_enable_event_detection(sensor_array[1].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
-  // gpio_enable_event_detection(sensor_array[1].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-   
-   interrupts_attach_handler(risingEdge_2);
-   interrupts_attach_handler(fallingEdge_2);
-
-//   gpio_enable_event_detection(sensor_array[2].echo_rising_pin, GPIO_DETECT_RISING_EDGE);
- //  gpio_enable_event_detection(sensor_array[2].echo_falling_pin, GPIO_DETECT_FALLING_EDGE);
-   
-   interrupts_attach_handler(risingEdge_1);
-   interrupts_attach_handler(fallingEdge_1);
-
-   interrupts_enable_source(INTERRUPTS_GPIO3);
-
-   interrupts_global_enable();
+    interrupts_enable_source(INTERRUPTS_GPIO3);
+    interrupts_global_enable();
 }
 
 void ultraSound_init(void){
 
     interrupts_init();
 
-    // change to a for loop over num sensors
-    gpio_set_output(sensor_array[0].trigger);
-  	gpio_set_input(sensor_array[0].echo_rising_pin);
-  	gpio_set_pulldown(sensor_array[0].echo_rising_pin);
-    gpio_set_input(sensor_array[0].echo_falling_pin);
-    gpio_set_pulldown(sensor_array[0].echo_falling_pin);
+    for(int i = 0; i < NUM_SENSORS; i++){
+        gpio_set_output(sensor_array[i].trigger);
+        gpio_set_input(sensor_array[i].echo_rising_pin);
+        gpio_set_pulldown(sensor_array[i].echo_rising_pin);
+        gpio_set_input(sensor_array[i].echo_falling_pin);
+        gpio_set_pulldown(sensor_array[i].echo_falling_pin);
+    }
 
-  	gpio_set_output(sensor_array[1].trigger);
-  	gpio_set_input(sensor_array[1].echo_rising_pin);
-  	gpio_set_pulldown(sensor_array[1].echo_rising_pin);
-    gpio_set_input(sensor_array[1].echo_falling_pin);
-    gpio_set_pulldown(sensor_array[1].echo_falling_pin);
-
-	gpio_set_output(sensor_array[2].trigger);
-  	gpio_set_input(sensor_array[2].echo_rising_pin);
-  	gpio_set_pulldown(sensor_array[2].echo_rising_pin);
-    gpio_set_input(sensor_array[2].echo_falling_pin);
-    gpio_set_pulldown(sensor_array[2].echo_falling_pin);
-
-	timer_delay_ms(40);
-
-      send_new_pulse(sensor_array[0].trigger);
-   // send_new_pulse(sensor_array[1].trigger);
-   // send_new_pulse(sensor_array[2].trigger);
+    timer_delay_ms(40);
+    send_new_pulse(sensor_array[0].trigger);
 }
 
 unsigned int getDistance(int pos){
-  // Out of bounds  
-  if(pos > (NUM_SENSORS - 1)) return -1;
+    // Out of bounds  
+    if(pos > (NUM_SENSORS - 1)) return -1;
 
-  return distances[pos];    
+    return distances[pos];    
 }
 
 
